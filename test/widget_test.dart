4fb884c4603d2c24f97886d99d87b090d90c66c8
Muhammad Hamza Shahid <mr.hamza.shahid@gmail.com>
2020-10-20 12:41:47 +0500

    Synced the project with course

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pak_destini/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(PakDestini());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
