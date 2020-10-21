9c5613f09afab8e9dbc0900401ae4996fa13ee35
Muhammad Hamza Shahid <mr.hamza.shahid@gmail.com>
2020-10-21 11:28:10 +0500

    Final Commit
    added conditions to make the application responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+import 'package:pak_destini/story_brain.dart';
 
 void main() {
   runApp(PakDestini());
@@ -15,7 +14,8 @@ class PakDestini extends StatelessWidget {
     );
   }
 }
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+
+StoryBrain storyBrain = new StoryBrain();
 
 class StoryPage extends StatefulWidget {
   @override
@@ -23,69 +23,81 @@ class StoryPage extends StatefulWidget {
 }
 
 class _StoryPageState extends State<StoryPage> {
+  bool isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-
         decoration: new BoxDecoration(
-          image: new DecorationImage(image: new AssetImage('images/background.png'),fit: BoxFit.cover,),
+          image: new DecorationImage(
+            image: new AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-
-
-      padding: EdgeInsets.symmetric(vertical: 50,horizontal: 50),
+        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
-          child: Column(children: [
-            Expanded(flex:12,
-              child: Center(
-                child: Text(
-                  //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                  "Story Text will go here",style: TextStyle(
-                  fontSize: 25.0,
-                ),),
-              ),),
-            Expanded(
-              flex: 2,
-              child: FlatButton(
-                onPressed: () {
-                  //Choice 1 made by user.
-                  //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                },
-                color: Colors.red,
-                child: Text(
-                  //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                  'Choice 1',
-                  style: TextStyle(
-                    fontSize: 20.0,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
                   ),
                 ),
               ),
-            ), SizedBox(
-          height: 20.0,
-        ),
-          Expanded(
-            flex: 2,
-            //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-            //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-            child: FlatButton(
-              onPressed: () {
-                //Choice 2 made by user.
-                //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-              },
-              color: Colors.blue,
-              child: Text(
-                //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                'Choice 2',
-                style: TextStyle(
-                  fontSize: 20.0,
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                      isVisible = storyBrain.buttonVisibility();
+                    });
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          )],),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                        isVisible = storyBrain.buttonVisibility();
+                      });
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  visible: isVisible,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
